94c1cdcc31e443819a60791c0af32346d3984765
harim-000 <halimseok@gmail.com>
2023-07-25 10:33:03 +0900

    Feat: auth/logout

--- lecture-vue-trello/trello-sample/src/App.vue
+++ lecture-vue-trello/trello-sample/src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div id="app">
-    여기 시작점
+     <Navbar />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Navbar from './components/NavBar.vue'
+
 export default {
-  
+  name: 'app',
+  components: { Navbar },
+  data () {
+    return {
+    }
+  }
 }
 </script>
 
